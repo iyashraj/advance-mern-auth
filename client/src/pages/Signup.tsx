28cb6efd8d596ea299b91d7b28ceac27253c60a6
iyashraj <yashr3037@gmail.com>
2024-01-17 22:45:22 +0530

    fix server routing

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup: React.FC = () => {
-  return <div>Signup</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-center font-semibold my-7">Signup</h1>
+      <input />
+      <input />
+      <input />
+      <button>Sign Up</button>
+      <Link to="/signin">Have an account</Link>
+    </div>
+  );
 };
 
 export default Signup;
