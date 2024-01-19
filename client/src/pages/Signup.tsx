c689fac41c4106c80b3c9f24660dba775356043c
iyashraj <yashr3037@gmail.com>
2024-01-19 23:24:57 +0530

    signup page functionalities completed

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -2,11 +2,13 @@ import React, { useState, ChangeEvent, FormEvent } from "react";
 import { Link } from "react-router-dom";
 
 interface FormData {
-  [key:string] : string | undefined;
+  [key: string]: string | undefined;
 }
 
 const Signup: React.FC = () => {
   const [formData, setFormData] = useState<FormData>({});
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
@@ -15,47 +17,59 @@ const Signup: React.FC = () => {
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-
+    setIsLoading(true);
+    setError(false);
     try {
-      const res = await fetch("http://localhost:3000/api/auth/signup", formData);
-
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      console.log(res);
       if (!res.ok) {
-        throw new Error('Failed to sign up');
+        setIsLoading(false);
+        setError(true);
+        throw new Error("Failed to sign up");
       }
 
       const data = await res.json();
       console.log(data);
-    } catch (error) {
-      console.error(error.message);
+    } catch (error: any) {
+      setIsLoading(false);
+      setError(true);
     }
   };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Signup</h1>
-      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>  
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Username"
           className="bg-slate-100 p-3 rounded-lg border-slate-500"
           id="username"
-          onClick={handleChange}
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="Email Address"
           className="bg-slate-100 p-3 rounded-lg"
           id="email"
-          onClick={handleChange}
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="Password"
           className="bg-slate-100 p-3 rounded-lg"
           id="password"
-          onClick={handleChange}
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          disabled={isLoading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {isLoading ? "Loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -65,6 +79,7 @@ const Signup: React.FC = () => {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-500">Something went wrong!</p>}
     </div>
   );
 };
